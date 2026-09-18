d5be314643b55dcd9fde5b9c439d8bd4b0614304
Krushna chandre <krushnachandre5448@gmail.com>
2026-09-18 22:41:31 +0530

    Add function to find the largest number in array

--- Sample.js
+++ Sample.js
@@ -1,1 +1,12 @@
 
+let numbers = [25, 10, 45, 30, 60, 15];
+
+let largest = numbers[0];
+
+for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > largest) {
+        largest = numbers[i];
+    }
+}
+
+console.log("Largest number is:", largest);
